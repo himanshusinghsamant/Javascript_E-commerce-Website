61531f784a00585e351c2664e3441cc3a2942caa
Himanshu <himanshusamant3@gmail.com>
2023-05-05 12:41:11 +0530

    change in increment and decrement button od sproduct, change in size of  signup input

--- Sproduct.js
+++ Sproduct.js
@@ -9,14 +9,17 @@ function get_items() {
 get_items();
 
 // --------> function for increment the quantity of the selected product ---------------->
+
 function increment() {
   const Rupees = document.getElementById("rupees");
+  const minus = document.getElementById("btn-1");
   const plus = document.getElementById("btn-2");
   plus.addEventListener("click", () => {
     let value = document.getElementById("value");
     let val = (value = value.innerHTML++);
     if (val >= 1) {
-      Rupees.innerHTML = parseFloat(Rupees.innerHTML) * 2;
+      Rupees.innerHTML = parseFloat(Rupees.innerHTML) + 450;
+      minus.style.display = "initial";
     }
 
     if (value === 4) {
@@ -35,16 +38,18 @@ function decrement() {
   const plus = document.getElementById("btn-2");
 
   minus.addEventListener("click", () => {
-    let value = document.getElementById("value")
+    let value = document.getElementById("value");
     let val = (value = value.innerHTML--);
-    console.log(val)
-    Rupees.innerHTML = parseFloat(Rupees.innerHTML) / 2;
+    // console.log(val)
+    Rupees.innerHTML = parseFloat(Rupees.innerHTML) - 450;
     if (value <= 2) {
       Rupees.innerHTML = 450;
     }
-    if (value <= 1) {
-      value = 0
+    if (value <= 2) {
+      // console.log((value = 0));
+      minus.style.display = "none";
     }
+
     if (value === 5) {
       plus.style.backgroundColor = " aquamarine";
       plus.style.color = "black";
@@ -75,26 +80,20 @@ function add_to_cart() {
     let product_name = document.getElementById("pro-name").innerHTML;
     const Rupees = document.getElementById("rupees").innerHTML;
     let value = document.getElementById("value").innerHTML;
-    
-    let store_pro = localStorage.getItem('itemJson')
-    if (store_pro == null){
-      productItemsArr = []
-      productItemsArr.push([main_image,product_name,Rupees,value])
-      localStorage.setItem('itemJson', JSON.stringify(productItemsArr))
-      
+
+    let store_pro = localStorage.getItem("itemJson");
+    if (store_pro == null) {
+      productItemsArr = [];
+      productItemsArr.push([main_image, product_name, Rupees, value]);
+      localStorage.setItem("itemJson", JSON.stringify(productItemsArr));
+    } else {
+      store_pro = localStorage.getItem("itemJson");
+      productItemsArr = JSON.parse(store_pro);
+      productItemsArr.push([main_image, product_name, Rupees, value]);
+      localStorage.setItem("itemJson", JSON.stringify(productItemsArr));
     }
-    else{
-      store_pro = localStorage.getItem('itemJson')
-      productItemsArr = JSON.parse(store_pro)
-      productItemsArr.push([main_image,product_name,Rupees,value])
-      localStorage.setItem('itemJson', JSON.stringify(productItemsArr))
-      
-    }
-    console.log(productItemsArr)
+    console.log(productItemsArr);
   });
   add_size();
-  
 }
 add_to_cart();
-
- 
